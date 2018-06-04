c12f6dbe62b2b9b2f7aa57e2b6693c8076d97e8f
manishyadav <yadav.ma@husky.neu.edu>
2018-06-03 22:16:32 -0400

    Integrating Spring Boot with MySQL

---
+++ README.md
@@ -0,0 +1,7 @@
+## Integrating Spring Boot with MySQL
+
+[Insert a static hello message](http://cs5200-summer2018-yadav.us-east-2.elasticbeanstalk.com/api/hello/insert)
+
+[Insert a parameterized hello message](http://cs5200-summer2018-yadav.us-east-2.elasticbeanstalk.com/api/hello/insert/hello)
+
+[Retrieve all hello messages](http://cs5200-summer2018-yadav.us-east-2.elasticbeanstalk.com//api/hello/select/all)
--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -1,10 +1,17 @@
 package edu.northeastern.cs5200.controllers.hello;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    HelloRepository helloRepository;
 
     @RequestMapping("/api/hello/string")
     public String sayHello() {
@@ -15,6 +22,25 @@ public class HelloController {
     public HelloObject sayHelloObject() {
         HelloObject obj = new HelloObject("Hello Manish Yadav!");
         return obj;
+    }
+
+    @RequestMapping("/api/hello/insert")
+    public HelloObject insertHelloObject(){
+        HelloObject obj = new HelloObject("\"Hello Manish Yadav!\"");
+        helloRepository.save(obj);
+        return obj;
+    }
+
+    @RequestMapping("/api/hello/insert/{msg}")
+    public HelloObject insertMessage(@PathVariable ("msg") String message){
+        HelloObject obj = new HelloObject(message);
+        helloRepository.save(obj);
+        return obj;
+    }
+
+    @RequestMapping("/api/hello/select/all")
+    public List<HelloObject> selectAllHelloObject(){
+        return helloRepository.findAll();
     }
 
 }
